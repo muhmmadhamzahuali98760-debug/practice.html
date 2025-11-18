23f6ae6dc73cd8f1ff6f0387bb5c70d947fafb6e
muhmmadhamzahuali98760-debug <muhmmadhamzahuali98760@gmail.com>
2025-11-18 19:23:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstname=prompt("Enter Your First Name")
+console.log("Hello" +firstname)
+
+let Lastname=prompt("Enter Your Last Name")
+console.log("Hello" +Lastname)
+
+let fullname=prompt("Enter Your fullname ")
+console.log("Hello" +fullname)
+
+let age =prompt("Enter Your age ")
+console.log("Hello" +age)
+
+let city=prompt("Enter Your city ")
+console.log("Hello" +city)
+
+let country=prompt("Enter Your country ")
+console.log("Hello" +country)
+
+let collagename=prompt("Enter Your collagename ")
+console.log("Hello" +collagename)
+
+let favoritSubject=prompt("Enter Your favoritSubject ")
+console.log("Hello" +favoritSubject)
+
+let yearOfBirth=prompt("Enter Your  yearOfBirth ")
+console.log("Hello" + yearOfBirth)
+
+let currentYear=prompt("Enter Your currentYear ")
+console.log("Hello" +currentYear)
